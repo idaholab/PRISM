a4fd795e9a56573fbaf27a32d017f0e62bbd8e2e
Sterbentz <stermj@caes.lan>
2017-05-31 16:50:38 -0600

    Initial commit for VolumeVisualization

---
+++ VolumeVisualization/Assets/Scripts/ArcBallCamera.cs
@@ -0,0 +1,118 @@
+﻿// Adapted from a script at: http://wiki.unity3d.com/index.php?title=MouseOrbitImproved 
+using UnityEngine;
+using System.Collections;
+ 
+[AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
+public class ArcBallCamera : MonoBehaviour
+{
+
+    public Transform target;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    public float yMinLimit = -90f;
+    public float yMaxLimit = 90f;
+
+    public float distanceMin = .5f;
+    public float distanceMax = 15f;
+
+    private Rigidbody rigidbody;
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        rigidbody = GetComponent<Rigidbody>();
+
+        // Make the rigid body not change rotation
+        if (rigidbody != null)
+        {
+            rigidbody.freezeRotation = true;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (target && Input.GetMouseButton(0))
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+
+            RaycastHit hit;
+            if (Physics.Linecast(target.position, transform.position, out hit))
+            {
+                distance -= hit.distance;
+            }
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
+
+/******************** OLD SCRIPT **************/
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ArcBallCamera : MonoBehaviour {
+
+//    public GameObject target;
+//    public float rotationSpeed = 5;
+
+//    public float speedH = 2.0f;
+//    public float speedV = 2.0f;
+
+//    private float yaw = 0.0f;
+//    private float pitch = 0.0f;
+
+//	// Use this for initialization
+//	void Start () {
+//        transform.LookAt(target.transform.position);
+//    }
+
+//	// Update is called once per frame
+//	void Update () {
+//        //transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+
+//        if (Input.GetMouseButton(0))
+//        {
+//            transform.LookAt(target.transform.position);
+//            transform.RotateAround(target.transform.position, Vector3.up, Input.GetAxis("Mouse X") * rotationSpeed);
+//            transform.RotateAround(target.transform.position, Vector3.right, Input.GetAxis("Mouse Y") * rotationSpeed);
+//        }
+//        // first person camera
+//        //if (Input.GetMouseButton(0))
+//        //{
+//        //    yaw += speedH * Input.GetAxis("Mouse X");
+//        //    pitch -= speedV * Input.GetAxis("Mouse Y");
+
+//        //    transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+//        //}
+//    }
+//}
---
+++ VolumeVisualization/Assets/Scripts/RAWFileMapper.cs
@@ -0,0 +1,62 @@
+﻿// Written by stermj
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class RAWFileMapper : MonoBehaviour {
+
+    private Texture3D data;
+
+    private int width = 256;      // X dimension
+    private int height = 256;     // Y dimension
+    private int depth = 128;      // Z dimension
+
+    private Color[] scalars;
+
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        FileStream volumeDataFile = new FileStream("Assets/Data/VisMale.raw", FileMode.Open);
+        load8BitRawFile(volumeDataFile);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Loads the given 8-bit .raw file into a Texture3D data structure
+    private void load8BitRawFile(FileStream file)
+    {
+        // Initialize the Texture3D of data
+        data = new Texture3D(width, height, depth, TextureFormat.RGBA32, false);
+
+        // Read the raw file into a buffer of bytes
+        BinaryReader reader = new BinaryReader(file);
+        byte[] buffer = new byte[width * height * depth];
+        int size = sizeof(byte);
+        reader.Read(buffer, 0, size * buffer.Length);
+        reader.Close();
+        
+        // Scale the scalar values to [0, 1]
+        scalars = new Color[buffer.Length];
+        for(int i = 0; i < buffer.Length; i += 4)
+        {
+            scalars[i] = new Color(((float) buffer[i] / byte.MaxValue), 0, 0, 1);
+        }
+
+        // Put the intensity scalar values into the Texture3D
+        data.SetPixels(scalars);
+        data.Apply();
+
+        // Send the intensity scalar values to the shader
+        rend = GetComponent<Renderer>();
+        rend.material.SetTexture("_VolumeDataTexture", data);
+
+    }
+
+}
